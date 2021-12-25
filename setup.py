1d94106480213a7d1f510bf8f7f3a1cb621aebcb
supplepentan <pentagramog@gmail.com>
2021-12-26 07:51:23 +0900

    update

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-import subprocess
-
-subprocess.run(["python", "-m", "pip", "install", "-U", "pip", "setuptools"])
-subprocess.run(["python", "-m", "pip", "install", "-r", "requirements.txt"])
-subprocess.run(["python", "-m", "pip", "install", "torch==1.10.1+cu113", "torchvision==0.11.2+cu113", "-f", "https://download.pytorch.org/whl/cu113/torch_stable.html"])
-
-
 import torch
+import os
+
+
+input = "input"
+output = "output"
+
+if not os.path.exists(input):
+    os.mkdir("input")
+if not os.path.exists(output):
+    os.mkdir("output")
+
 model = torch.hub.load("PeterL1n/RobustVideoMatting",
                        "mobilenetv3").cuda()  # or "resnet50"
 convert_video = torch.hub.load("PeterL1n/RobustVideoMatting", "converter")
