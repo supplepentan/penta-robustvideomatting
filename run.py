0f0fe0b9b4c51042e62d085f1988b4291aab1bbf
supplepentan <pentagramog@gmail.com>
2021-12-26 09:27:45 +0900

    update

--- run.py
+++ run.py
@@ -1,14 +1,23 @@
 from inference import convert_video
 import torch
+import glob
+import os
+import shutil
 
-convert_video(
-    model = torch.hub.load("PeterL1n/RobustVideoMatting", "mobilenetv3").cuda(),                           # 模型，可以加载到任何设备（cpu 或 cuda）
-    input_source='input/input.mp4',        # 视频文件，或图片序列文件夹
-    output_type='video',             # 可选 "video"（视频）或 "png_sequence"（PNG 序列）
-    output_composition='output/com.mp4',    # 若导出视频，提供文件路径。若导出 PNG 序列，提供文件夹路径
-    output_alpha="output/pha.mp4",          # [可选项] 输出透明度预测
-    output_foreground="output/fgr.mp4",     # [可选项] 输出前景预测
-    output_video_mbps=4,             # 若导出视频，提供视频码率
-    downsample_ratio=None,           # 下采样比，可根据具体视频调节，或 None 选择自动
-    seq_chunk=12,                    # 设置多帧并行计算
-)
+for movie in glob.glob("input/*.mp4"):
+    basename = os.path.splitext(os.path.basename(movie))[0]
+    basename_ex = os.path.basename(movie)
+    if not os.path.exists(os.path.join("output", basename)):
+        os.mkdir(os.path.join("output", basename))
+    shutil.copy(movie, os.path.join("output", basename, basename_ex))
+    convert_video(
+        model = torch.hub.load("PeterL1n/RobustVideoMatting", "mobilenetv3").cuda(),
+        input_source=movie,
+        output_type='video',
+        output_composition=os.path.join("output", basename, "com.mp4"),
+        output_alpha=os.path.join("output", basename, "pha.mp4"),
+        output_foreground=os.path.join("output", basename, "fgr.mp4"),
+        output_video_mbps=4,
+        downsample_ratio=None,
+        seq_chunk=12,
+    )
